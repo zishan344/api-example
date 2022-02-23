3e3281d36813b436ded587d5270839fdfc7a2962
zishan344 <zishanahmed344@gmail.com>
2022-02-23 19:25:24 +0600

    done the todays module

---
+++ quiz.js
@@ -0,0 +1,8 @@
+const bondCode = ` I am Fake James bond . My new code is: 00${7 + 1 + 2}`;
+console.log(bondCode);
+const data = { result: [{ userName: { title: "Mr.", name: "sakib khan" } }] };
+console.log(data.result[0].userName.name);
+const info = { name: "", phoneNumber: null };
+console.log(JSON.stringify(info));
+const array = { hobbies: ["dancing", "singing", "acting"] };
+console.log(JSON.stringify(array));
